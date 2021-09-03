337bc64e686fde3f11cb1bff776e408d1b6a0720
Muhammad Anas <79513367+Muhammadanas215@users.noreply.github.com>
2021-09-03 11:06:38 +0500

    Add files via upload

---
+++ class 2/src/component/greeting.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+
+ export default function Greeting({userName, rollNo}){
+    return(
+      <div>
+        <h1>Hello {userName}</h1>
+        <p> My Roll     No {rollNo} loremsadad asdasd dd ada sdas ada a s a a das da a</p>
+      </div>
+    )
+  }
---
+++ class 2/src/index.js
@@ -0,0 +1,21 @@
+import { directive } from "@babel/types";
+import React from "react";
+import ReactDOM from "react-dom"; 
+import Greeting from "./component/greeting"
+
+// let fb = React.createElement("a", {"href" : "https://www.facebook.com/"}, "facebook")
+// let h1 = React.createElement("h1", null, fb)
+// ReactDOM.render(h1,document.getElementById("root"))
+
+// function Greeting(){
+//   let h1 = React.createElement("h1", null, "Hello Anas");
+//   let p = React.createElement('p', {class: "text"}, 'loremsadad asdasd dd ada sdas ada a s a a das da a')
+//   return (
+//     React.createElement('div', null, h1, p)
+//   )
+
+// }
+// ReactDOM.render(<Greeting />, document.getElementById('root'))
+
+
+ReactDOM.render(<Greeting userName = "Anas" rollNo = "123456" />, document.getElementById('root'))
