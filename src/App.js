6c91cfa34bcb4c86df3ca9ec33b7e7acdad310d3
Muhammad Anas <79513367+Muhammadanas215@users.noreply.github.com>
2021-09-20 10:11:50 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Story from "./component/story"
+import SelfCounter from "./component/self-counter";
+import GoogleSearchResult from "./component/googlesearch/google-search-result"
+
+
+
+// function App() {
+//   return (
+//     <div>
+//    <Story  
+//     heading="Karachi"
+//       descp = "City Of Pakistan"
+//       para="Karachi is the largest city in Pakistan and the twelfth-largest city in the world. It is the capital of the Pakistani province of Sindh. Ranked as a beta-global city, it is Pakistan's premier industrial and financial centre, with an estimated GDP of $164 billion as of 2019."/>
+//       <Story  
+//     heading="Karachi"
+//       descp = "City Of Pakistan"
+//       para="Karachi is the largest city in Pakistan and the twelfth-largest city in the world. It is the capital of the Pakistani province of Sindh. Ranked as a beta-global city, it is Pakistan's premier industrial and financial centre, with an estimated GDP of $164 billion as of 2019."/>
+//       <Story  
+//     heading="Karachi"
+//       descp = "City Of Pakistan"
+//       para="Karachi is the largest city in Pakistan and the twelfth-largest city in the world. It is the capital of the Pakistani province of Sindh. Ranked as a beta-global city, it is Pakistan's premier industrial and financial centre, with an estimated GDP of $164 billion as of 2019."/>
+//       <Story  
+//     heading="Karachi"
+//       descp = "City Of Pakistan"
+//       para="Karachi is the largest city in Pakistan and the twelfth-largest city in the world. It is the capital of the Pakistani province of Sindh. Ranked as a beta-global city, it is Pakistan's premier industrial and financial centre, with an estimated GDP of $164 billion as of 2019."/>
+//       <Story  
+//     heading="Karachi"
+//       descp = "City Of Pakistan"
+//       para="Karachi is the largest city in Pakistan and the twelfth-largest city in the world. It is the capital of the Pakistani province of Sindh. Ranked as a beta-global city, it is Pakistan's premier industrial and financial centre, with an estimated GDP of $164 billion as of 2019."/>
+      
+//     </div>
+//   );
+// }
+
+
+// function App(){
+//   return(
+//     < SelfCounter />
+//   )
+// }
+ 
+function App(){
+  return(
+    <GoogleSearchResult />
+  )
+}
+
+export default App;
---
+++ src/component/googlesearch/google-search.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './google-search.css';
+import GoogleSearchResult from "./google-search-result"
+
+
+
+
+function GoogleSearch({link, url, title, description}) {
+return(
+<div className="container">
+<a className='main-a' href={link}>
+    <h3 className="h3">{url}</h3>
+    <h1 className="h1">{title}</h1>
+</a>
+<p className="p">{description}</p>
+</div>
+)
+}
+
+
+
+
+export default GoogleSearch;
